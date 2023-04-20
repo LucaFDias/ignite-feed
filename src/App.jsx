e16d3eabaca21e3eb9ed6c3fb7a90623d4d52804
Lucas Dias <lucasferndias@gmail.com>
2023-04-20 17:19:55 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import styles from './App.module.css';
 import Header from './components/Header';
-
+import { Posts } from './components/Posts';
+import { Sidebar } from './components/Sidebar';
 import './global.css';
-
 function App() {
   return (
     <div>
       <Header/>
-      <h1>Hello World</h1>
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar/>
+        </aside>
+        <main>
+          <Posts
+          author="Lucas Dias"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis ut 
+          reprehenderit doloribus. Libero qui aliquam repellendus labore numquam rerum hic animi 
+          aliquid, laboriosam accusantium odit modi doloremque, perspiciatis corporis molestiae?"
+          />
+        </main>
+      </div>
     </div>
   )
 }
